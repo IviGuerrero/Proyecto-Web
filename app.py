6148f379e32929d683b33f9e2c23dfbf9ba42761
IviGuerrero <ivanlucasguerrero@gmail.com>
2024-07-28 19:32:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def about():
     return render_template ('about.html')
 
+@app.route ('/index')
+def index():
+    return render_template ('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
